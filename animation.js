440c3ab5dabaaf41a750b72dc38e94ebfbfbd3be
mandanomena <andrianantenainamanda8@gmail.com>
2024-05-10 20:11:02 +0300

    portfolio

---
+++ animation.js
@@ -0,0 +1,16 @@
+var nbDrop = 1000;
+
+function randRange(minNum, maxNum) {
+    return (Math.floor(Math.random()*(maxNum - minNum + 1)) + minNum);
+}
+
+function createRain() {
+    for (i = 1; i< nbDrop; i++){
+        var dropleft = randRange(0, 1555);
+        var dropTop = randRange(-1000,1350);
+
+        $('.rain').append('<div class="drop" id="drop'+ i +'"></div>');
+        $('#drop' + i).css('left', dropleft);
+        $('#drop' + i).css('top', dropTop);
+    }
+}
